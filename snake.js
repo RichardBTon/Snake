a50ce0146dd3e6a91b3ac18155dbf45f3adcac56
RichardBTon <richard.btondel@gmail.com>
2020-10-04 15:55:16 +0200

    kan nå flytte mer snake-aktig

--- snake.js
+++ snake.js
@@ -1,48 +1,72 @@
-const fart = 20;
+const snakeBox = document.getElementById("snakeBox");
+const snakeHead = document.getElementById("snakeHead");
+
+const gridSize = 20;
+const pxPerRute = snakeBox.offsetHeight / gridSize;
+console.log(pxPerRute);
+
+let lastVisited = [];
+let grid = [];
+let fart = 1;
 let vx = 0;
 let vy = 0;
 
 let px = 5;
 let py = 5;
 
-const snakeBox = document.getElementById("snake-box");
-const snakeHead = document.getElementById("snakeHead");
+function rutenettInit(rutenett) {
+  const row = [];
+  row.length = gridSize;
+  row.fill(0);
+  for (var i = 0; i < gridSize; i++) {
+    rutenett.push(row);
+  }
+}
+rutenettInit(grid);
 
 document.addEventListener("keydown", moveInit);
 
+let started = false;
 function moveInit(event) {
   // left: 37
   // up: 38
   // right: 39
   // down: 40
+  if (!started) {
+    hello = setInterval(changePosition, 100);
+    started = true;
+  }
 
   if (event.keyCode === 37) {
     console.log("left");
     vx = -fart;
     vy = 0;
-    changePosition(vx, vy);
   }
   if (event.keyCode === 38) {
     console.log("up");
     vy = -fart;
     vx = 0;
-    changePosition(vx, vy);
   }
   if (event.keyCode === 39) {
     console.log("right");
     vx = fart;
     vy = 0;
-    changePosition(vx, vy);
   }
   if (event.keyCode === 40) {
     console.log("down");
     vy = fart;
     vx = 0;
-    changePosition(vx, vy);
+  }
+  if (event.keyCode === 32) {
+    console.log("pause");
+    vy = 0;
+    vx = 0;
+    clearInterval(hello);
+    started = false;
   }
 }
 
-function changePosition(vx, vy) {
-  snakeHead.style.left = snakeHead.offsetLeft + vx + "px";
-  snakeHead.style.top = snakeHead.offsetTop + vy + "px";
+function changePosition() {
+  snakeHead.style.left = snakeHead.offsetLeft + vx * pxPerRute + "px";
+  snakeHead.style.top = snakeHead.offsetTop + vy * pxPerRute + "px";
 }
